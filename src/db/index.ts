3cf47bfe4c6405e1a896e29070e9123f93b05f2a
najif jawoad jim <najifjawoad@gmail.com>
2026-05-23 15:48:44 +0600

    solved pools problem

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,7 @@
 import { Pool } from "pg";
 import config from "../config";
 
-export const pool = new Pool({
-    connectionString:config.connection_string,
-   
-})
+
 
 export const initDB = async() =>{
     try {
