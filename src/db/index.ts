487a94bd600e70025cad5b615c1911fb2a3db6fa
najif jawoad jim <najifjawoad@gmail.com>
2026-05-23 15:56:20 +0600

    implemented global error handler

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,10 @@
 import { Pool } from "pg";
 import config from "../config";
 
-
+export const pool = new Pool({
+    connectionString:config.connection_string,
+   
+})
 
 export const initDB = async() =>{
     try {
