39eee93603a1fa401d0df915cdfc6ae4f66e2862
najif jawoad jim <najifjawoad@gmail.com>
2026-05-23 16:02:40 +0600

    Improve API response formatting consistency

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -8,11 +8,7 @@ const loginUser = async(req:Request,res:Response) =>{
     const result = await authService.loginUserIntoDB(req.body);
 
     
-    
-     res.status(200).json({
-      success: true,
-      message: "User login successfully!",
-      data: result,
+  
     });
    } catch (error :any) {
     res.status(500).json({
